b646383db3dc1fbfe0ed43b10e0fb8e221da9e45
Nancy Novotná <d23974@oa-opava.cz>
2025-11-21 07:38:26 +0100

    optimalizace zmeny jazyku

--- myvali-stranka/jazyky.js
+++ myvali-stranka/jazyky.js
@@ -1,91 +1,54 @@
 
-
-
-function jazyk_CZ() {
-
+//INDEX
+document.addEventListener('DOMContentLoaded', function jazyk_CZ() {
     localStorage.setItem('jazyk', 'CZ')
-
     if (document.querySelector('title').innerHTML === 'Raccoon website') {
         document.querySelector('title').innerHTML = 'Mývalí stránka'
-
-        // ZÁHLAVÍ
+    
+// ZÁHLAVÍ
         document.querySelector('.zahlavi_domu').innerHTML = 'Domů'
         document.querySelector('.zahlavi_videa').innerHTML = 'Videa'
         document.querySelector('.zahlavi_fotky').innerHTML = 'Fotky'
-        document.querySelector('.zahlavi_hudbicka').innerHTML = 'Hudbička'
+        document.querySelector('.zahlavi_hudbicka').innerHTML = 'Hudbička'            
+// INDEX
+        document.querySelector('.nadpis_main').innerHTML = 'Mýval Severní'
+        document.querySelector('.podnadpis_main').innerHTML = 'Náhodná stránka a mývalech,
+        document.querySelector('.videolist_1').innerHTML = 'Mýval, co robí bordel'
+        document.querySelector('.videolist_2').innerHTML = 'Útočný mýval'
+        document.querySelector('.videolist_3').innerHTML = 'Mývalí okupace'
+// VIDEA
+        document.querySelector('.nadpis_videa').innerHTML = 'VIDEA'
+// FOTKY
+        document.querySelector('.nadpis_fotky').innerHTML = 'FOTKY'
+    }
+});
+        
 
-        //INDEX
-        if (document.querySelector('.nadpis_main')) {
-            document.querySelector('.nadpis_main').innerHTML = 'Mýval Severní'
-        }
-        if (document.querySelector('.podnadpis_main')) {
-            document.querySelector('.podnadpis_main').innerHTML = 'Náhodná stránka a mývalech, protože proč ne.'
-        }
-        if (document.querySelector('.videolist_1')) {
-            document.querySelector('.videolist_1').innerHTML = 'Mýval, co robí bordel'
-        }
-        if (document.querySelector('.videolist_2')) {
-            document.querySelector('.videolist_2').innerHTML = 'Útočný mýval'
-        }
-        if (document.querySelector('.videolist_3')) {
-            document.querySelector('.videolist_3').innerHTML = 'Mývalí okupace'
-        }
-
-        // VIDEA
-        if (document.querySelector('.nadpis_videa')) {
-            document.querySelector('.nadpis_videa').innerHTML = 'VIDEA'
-        }
-
-        // FOTKY
-        if (document.querySelector('.nadpis_fotky')) {
-            document.querySelector('.nadpis_fotky').innerHTML = 'FOTKY'
-        }
-    };
-}
-
-function jazyk_EN() {
-
+document.addEvenetListener('DOMContentLoaded', function jazyk_EN() {
     localStorage.setItem('jazyk', 'EN')
-
     if (document.querySelector('title').innerHTML === 'Mývalí stránka') {
         document.querySelector('title').innerHTML = 'Raccoon website'
 
-        // ZÁHLAVÍ
+    // ZÁHLAVÍ
         document.querySelector('.zahlavi_domu').innerHTML = 'Home'
         document.querySelector('.zahlavi_videa').innerHTML = 'Videos'
         document.querySelector('.zahlavi_fotky').innerHTML = 'Photos'
         document.querySelector('.zahlavi_hudbicka').innerHTML = 'Music'
 
-        // INDEX
-        if (document.querySelector('.nadpis_main')) {
-            document.querySelector('.nadpis_main').innerHTML = 'Raccoon';
-        }
-        if (document.querySelector('.podnadpis_main')) {
-            document.querySelector('.podnadpis_main').innerHTML = 'Random web about raccoons because why not.';
-        }
-        if (document.querySelector('.videolist_1')) {
-            document.querySelector('.videolist_1').innerHTML = 'Raccoon making a mess';
-        }
-        if (document.querySelector('.videolist_2')) {
-            document.querySelector('.videolist_2').innerHTML = 'Aggressive raccoon';
-        }
-        if (document.querySelector('.videolist_3')) {
-            document.querySelector('.videolist_3').innerHTML = 'Raccoon invasion';
-        }
+    // INDEX
+        document.querySelector('.nadpis_main').innerHTML = 'Raccoon'
+        document.querySelector('.podnadpis_main').innerHTML = 'Random web about raccoons because why not.'
+        document.querySelector('.videolist_1').innerHTML = 'Raccoon making a mess'
+        document.querySelector('.videolist_2').innerHTML = 'Aggressive raccoon'
+        document.querySelector('.videolist_3').innerHTML = 'Raccoon invasion'
 
+    // VIDEA
+        document.querySelector('.nadpis_videa').innerHTML = 'VIDEOS'
 
-        //VIDEA
-        if (document.querySelector('.nadpis_videa')) {
-            document.querySelector('.nadpis_videa').innerHTML = 'VIDEOS'
-        }
-
-        // FOTKY
-        if (document.querySelector('.nadpis_fotky')) {
-            document.querySelector('.nadpis_fotky').innerHTML = 'PHOTOS'
-        }
-    };
-
-}
+    // FOTKY
+        document.querySelector('.nadpis_fotky').innerHTML = 'PHOTOS'
+    }
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     const ulozeny_jazyk = localStorage.getItem('jazyk');
